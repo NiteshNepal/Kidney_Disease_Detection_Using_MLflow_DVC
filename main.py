10801248deb96ac521ea4f7c96d7978e60e76304
NiteshNepal <niteshnepal101@gmail.com>
2025-03-15 17:59:30 +0530

    data ingestion added

---
+++ main.py
@@ -0,0 +1,16 @@
+from cnnClassifier import logger 
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f" >>>>>> Stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f" >>>>>> Stage {STAGE_NAME} completed <<<<<<")
+        
+except Exception as e:
+    logger.exception(e)
+    raise e 
+        
--- template.py
+++ template.py
@@ -24,7 +24,7 @@ list_of_files= [
     "requirements.txt",
     "setup.py",
     "research/trails.ipynb",
-    "template/index.html"
+    "template/index.html",
     
 ]
 
